551ad21c3ce2eabe3cb41aabd747866beb74e4b1
Neeharika Baral <neehabaral72@gmail.com>
2023-10-23 02:27:21 -0400

    turned into function & refractored using callback

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,24 @@
 const request = require('request');
-const argument = process.argv;
 
-const catBreed = argument[2]; // retrieving command lin arg from array
-const domain = 'https://api.thecatapi.com/v1/breeds/search?q=';
+const fetchBreedDescription = function (breedName, callback) {
+  const domain = 'https://api.themcatapi.com/v1/breeds/search?q=';
+  const URL = domain.concat(breedName);
 
-const URL = domain.concat(catBreed.trim());
-// concatenates cat breed with domain to complete the url
+  request(URL, (error, response, body) => {
+    if (error) {
+      callback(error, null)
+      return;
+    };
+    
+    if (body === '[]') {
+      callback(null, "This cat breed is not in 'thecatapi' database");
+      return;
+    };
 
-request(URL, (error, response, body) => {
-  if (error) {
-    console.log('error: ', error);
-  }
+    const catObject = JSON.parse(body).pop(0);
+    console.log(catObject['description']);
+    
+  });
+}
 
-  if (body === '[]') {
-    console.log("This cat breed is not in 'thecatapi' database");
-    return;
-  }
-
-  const catObject = JSON.parse(body).pop(0);
-  // changes string to object with JSON.parse -> then pops out object from array
-  console.log(catObject['description']);
-  
-});
+module.exports = fetchBreedDescription;
---
+++ index.js
@@ -0,0 +1,11 @@
+const fetchBreedDescription = require('./breedFetcher.js');
+const argument = process.argv;
+const breedName = argument[2];
+
+fetchBreedDescription(breedName, (error, description) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+    return;
+  }
+  console.log(description);
+});
