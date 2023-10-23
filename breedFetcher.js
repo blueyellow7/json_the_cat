c2f42e5b30d24a2e848eebfccf4d65ae75ff9220
Neeharika Baral <neehabaral72@gmail.com>
2023-10-23 01:36:42 -0400

    implemented code to fetch cat breed descriptions

---
+++ breedFetcher.js
@@ -0,0 +1,24 @@
+const request = require('request');
+const argument = process.argv;
+
+const catBreed = argument[2]; // retrieving command lin arg from array
+const domain = 'https://api.thecatapi.com/v1/breeds/search?q=';
+
+const URL = domain.concat(catBreed.trim());
+// concatenates cat breed with domain to complete the url
+
+request(URL, (error, response, body) => {
+  if (error) {
+    console.log('error: ', error);
+  }
+
+  if (body === '[]') {
+    console.log("This cat breed is not in 'thecatapi' database");
+    return;
+  }
+
+  const catObject = JSON.parse(body).pop(0);
+  // removes object from array
+  console.log(catObject['description']);
+  
+});
